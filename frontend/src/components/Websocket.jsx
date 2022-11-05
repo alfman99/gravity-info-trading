129fc2d5ada1beff02f1c3db198380eb779afc4a
Marc Capde <capasc5@gmail.com>
2022-11-05 20:40:18 +0200

    updated frontend

---
+++ frontend/src/components/Websocket.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import Card from './card';
+
+import { WebSocketContext } from "../contexts/WebSocketContext";
+
+// interface NewsArticle {
+//     url: string;
+//     content: string;
+//     asset: string;
+//     published_at: Date;
+//     sentiment ?: number;
+//     id?: number;
+// }
+export const Websocket = () =>{
+    const socket = useContext(WebSocketContext);
+    const [messages,setMessages] = useState([]);
+    useEffect(()=>{
+        socket.on('connect',()=>{
+            console.log('Connected!')
+        })
+        socket.on('newNewsArticle',(data) => {
+            console.log('onMessage event received');
+            console.log(data);
+            setMessages((prev) => [...prev, data]);
+        });
+        return () => {            
+            socket.off('connect')
+            //socket.off('newNewsArticle')
+        }
+    },[]);
+
+    return (
+        <>            
+            {messages && messages.map((e)=> {
+                return <Card object={e}></Card>
+            })}            
+        </>
+    );
+}
---
+++ frontend/src/contexts/WebSocketContext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+import {io, Socket } from "socket.io-client";
+
+export const socket = io('http://localhost:3000/news'); // context instance declaration
+export const WebSocketContext = createContext(socket);
+export const WebsocketProvider = WebSocketContext.Provider
