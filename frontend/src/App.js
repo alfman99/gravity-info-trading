fc683b9c93c9bfb38d2687a7a302e928d9831e3e
Marc Capde <capasc5@gmail.com>
2022-11-05 19:57:10 +0200

    added websocket Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,42 @@
 import TradeViewChart from 'react-crypto-chart';
 import './style.css';
 import React from 'react';
-import { Box } from '@chakra-ui/react'
-//import { Image } from '@chakra-ui/react'
 import Image from "./imgs/logo.png";
+import Card from './components/card';
+import { socket, WebsocketProvider } from './contexts/WebSocketContext';
+import { Websocket } from './components/Websocket';
 
 export default function App() {
+  const card ={
+    title: "Some sample Title",
+    sentiment: "positive"
+  }
+  const cardList = [card,card,card];
   return (   
     <div className="parent">
+      <WebsocketProvider value={socket}>
+        <Websocket/>
+      </WebsocketProvider>
       <div class="myheader">
         <img width="50px" src={Image}/>
       </div>
       <div class="pageContent">
         <div class="news" align="left">
-        
+          {/* <Card object={card}></Card> */}
+          {cardList && cardList.map((e)=> {
+            return <Card object={e}></Card>
+          })}
+          
         </div>
         <div class="chart">
           <h3>ETH/USDT</h3>
           <TradeViewChart
           containerStyle={{
-            minHeight: '300px',
-            minWidth: '400px',
+            minHeight: '600px',
+            minWidth: '500%',
+            //with: '100%',
             marginBottom: '30px',
+            marginLeft: '50px',
           }}
           pair="ETHUSDT"
           />
