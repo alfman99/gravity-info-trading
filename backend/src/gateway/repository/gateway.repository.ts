87163834f2966bc189b8e54e65473c386d031e09
alfman99 <43989698+alfman99@users.noreply.github.com>
2022-11-05 20:46:01 +0100

    send through ws only 250 chars of news

--- backend/src/gateway/repository/gateway.repository.ts
+++ backend/src/gateway/repository/gateway.repository.ts
@@ -7,6 +7,16 @@ export class GatewayRepository {
   constructor(private readonly gateway: MyGateway) {}
 
   async sendNewsArticleToAllClients(news: NewsArticle) {
-    this.gateway.server.emit('newNewsArticle', news);
+    this.gateway.server.emit(
+      'newNewsArticle',
+      new NewsArticle(
+        news.url,
+        news.content.substring(0, 250),
+        news.asset,
+        news.published_at,
+        news.sentiment,
+        news.id,
+      ),
+    );
   }
 }
